d2b052b73eefb26d37527e4720821514173dc323
Galahaaad <nidam.mamoune@gmail.com>
2025-10-25 15:37:11 +0200

    feat: create React UI App component with overlay styling

---
+++ src/ui/App.tsx
@@ -0,0 +1,10 @@
+import { DebugPanel } from './components/DebugPanel';
+import './App.scss';
+
+export function App() {
+  return (
+    <div className="ui-container">
+      <DebugPanel />
+    </div>
+  );
+}
